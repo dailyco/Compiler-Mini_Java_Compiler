9d17f89b836820a559527a93893439f3703e4437
YuJin-Kim <21700140@handong.edu>
2020-04-17 21:12:57 +0900

    build basic Source reader structure

--- src/main/java/scanner/SourceReader.java
+++ src/main/java/scanner/SourceReader.java
@@ -9,17 +9,20 @@ import java.util.List;
 public class SourceReader {
 	public String run(String path) {
 		StringBuffer sb = new StringBuffer();
+		List<String> lines = null;
 		
 		try {
-			List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
-			
-	        for(String line: lines)
-	            sb.append(line + "\n");
-	        sb.append('\255');
+			lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.err.print("File not found: Please check the file"); // TODO create file not found exception
+			return "";
 		}
+		
+		for(String line: lines)
+            sb.append(line + "\n");
+        sb.append('\255');
         
 		return sb.toString();
 	}
