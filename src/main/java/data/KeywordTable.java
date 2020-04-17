e1af7bb6ee28d3ffc669fa5676e54583d5ec6daf
YuJin-Kim <21700140@handong.edu>
2020-04-17 19:28:55 +0900

    build basic keyword table structure

--- src/main/java/data/KeywordTable.java
+++ src/main/java/data/KeywordTable.java
@@ -1,5 +1,31 @@
 package data;
 
 public enum KeywordTable {
+	// Keywords
+	IF("if"), ELSE("else"), FOR("for"), WHILE("while"), // Control keyword
+	CLASS("class"), INT("int"), // Data type
+	OUT_PRINTLN("out.println"); // Special keyword
+	
+	final private String keyword;
+	
+	private KeywordTable(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	public String getKeyword() {
+		return this.keyword;
+	}
+	
+	public int length() {
+		return this.keyword.length();
+	}
+	
+	public KeywordTable find(String src) {
+		for (KeywordTable keywordType : KeywordTable.values()) {
+			if (src.startsWith(keywordType.getKeyword()))
+				return keywordType;
+		}
 
+		return null;
+	}
 }
