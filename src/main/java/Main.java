822f9195110dd1af8d9901daae5f9108fbc7d9f6
YuJin-Kim <21700140@handong.edu>
2020-04-17 21:11:43 +0900

    add new Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import scanner.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner();
+		scanner.run(args);
+	}
+
+}
