7a7f6c0e47ef4a7b8747d99b139c5ceded5e1dc8
YuJin-Kim <21700140@handong.edu>
2020-04-17 19:35:10 +0900

    build basic LexicalAnalysisException structure

--- src/main/java/exception/LexicalAnalysisException.java
+++ src/main/java/exception/LexicalAnalysisException.java
@@ -1,5 +1,25 @@
 package exception;
 
-public class LexicalAnalysisException {
+@SuppressWarnings("serial")
+public class LexicalAnalysisException extends Exception {
+	private int errorPosition; // 오류가 발생한 위치 (line)
+	private String errorMessage; // 오류 메세지
 
+	public LexicalAnalysisException(int errorPosition) {
+		this.errorPosition = errorPosition;
+	}
+
+	public LexicalAnalysisException(int errorPosition, String errorMessage) {
+		this.errorPosition = errorPosition;
+		this.errorMessage = errorMessage;
+	}
+
+	public int getErrorPosition() {
+		return errorPosition;
+	}
+
+	@Override
+	public String getMessage() {
+		return "\nexception.LexicalAnalysisException:" + errorMessage;
+	}
 }
