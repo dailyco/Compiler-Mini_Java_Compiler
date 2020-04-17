325b48ca6abc914fade887578fc3905f2e904a76
YuJin-Kim <21700140@handong.edu>
2020-04-17 20:30:01 +0900

    build basic Source Reader structure

--- src/main/java/scanner/SourceReader.java
+++ src/main/java/scanner/SourceReader.java
@@ -1,5 +1,26 @@
 package scanner;
 
-public class SourceReader {
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class SourceReader {
+	public String run(String path) {
+		StringBuffer sb = new StringBuffer();
+		
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+			
+	        for(String line: lines)
+	            sb.append(line + "\n");
+	        sb.append('\255');
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		return sb.toString();
+	}
 }
