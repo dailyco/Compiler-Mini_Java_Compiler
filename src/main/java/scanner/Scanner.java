36290bf234096910ef5975e6112099adcdb490d9
YuJin-Kim <21700140@handong.edu>
2020-04-17 20:03:10 +0900

    build basic Scanner structure

--- src/main/java/scanner/Scanner.java
+++ src/main/java/scanner/Scanner.java
@@ -1,5 +1,33 @@
 package scanner;
 
-public class Scanner {
+import data.SymbolTable;
 
+public class Scanner {
+	
+	public static void main(String[] args) {
+		if (args[0] == null) {
+			System.err.print("Please enter the file path for scanner");
+			return;
+		}
+		
+		Scanner scanner = new Scanner();
+		String src = scanner.readSource(args[0]);
+		SymbolTable symbolTable = scanner.analysisTokens(src);
+		scanner.writeTokens(symbolTable);
+	}
+	
+	private String readSource(String path) {
+		SourceReader sourceReader = new SourceReader();
+		return sourceReader.run(path);
+	}
+	
+	private SymbolTable analysisTokens(String src) {
+		LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
+		lexicalAnalyzer.run(src);
+	}
+	
+	private void writeTokens(SymbolTable symbolTable) {
+		TokenWriter tokenWriter = new TokenWriter();
+		tokenWriter.run(symbolTable);
+	}
 }
