220f09567984bb939d54d4b25cc477fd731eebee
YuJin-Kim <21700140@handong.edu>
2020-04-17 21:11:24 +0900

    build up Scanner class

--- src/main/java/scanner/Scanner.java
+++ src/main/java/scanner/Scanner.java
@@ -1,25 +1,24 @@
 package scanner;
 
-import data.SymbolTable;
+import data.AnalyzedTokenList;
 
 public class Scanner {
 	private String src;
 	private String path;
 	private String fileName;
-	private SymbolTable symbolTable = new SymbolTable();
+	private AnalyzedTokenList tokenList = new AnalyzedTokenList();
 	
-	public static void main(String[] args) {
+	public void run(String[] args) {
 		if (args[0] == null) {
-			System.err.print("Please enter the file path for scanner");
+			System.err.print("Please enter the file path for scanner"); // TODO create argument not found exception
 			return;
 		}
 		
-		Scanner scanner = new Scanner();
-		
-		scanner.getInfos(args[0]);
-		scanner.readSource();
-		scanner.analysisTokens();
-		scanner.writeTokens();
+		getInfos(args[0]);
+		readSource();
+		if (src == "") return; // File not found exception
+//		analysisTokens();
+		writeTokens();
 	}
 	
 	private void getInfos(String path) {
@@ -34,13 +33,13 @@ public class Scanner {
 		src = sourceReader.run(path);
 	}
 	
-	private void analysisTokens() {
-		LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
-		symbolTable = lexicalAnalyzer.run(src);
-	}
+//	private void analysisTokens() {
+//		LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
+//		symbolTable = lexicalAnalyzer.run(src);
+//	}
 	
 	private void writeTokens() {
-		TokenWriter tokenWriter = new TokenWriter();
-		tokenWriter.run(symbolTable);
+		TokenWriter tokenWriter = new TokenWriter(fileName);
+		tokenWriter.run(tokenList, false);
 	}
 }
